00a5bc396fb95fb1ae169af1fea83d75e745d8cd
Petar Popovski <ppopovski53@gmail.com>
2022-04-22 15:16:25 +0200

    initial commit

---
+++ src/test/java/com/petarangela/wineeshop/WineEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petarangela.wineeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
